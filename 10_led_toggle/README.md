7152982d2caea2b3160cf29f9d2735196014f985
Leonid Vygovskiy <leonid.vygovskiy@gmail.com>
2022-05-11 11:53:13 +0300

    10 led toggle

---
+++ 10_led_toggle/README.md
@@ -0,0 +1,4 @@
+## Changes
+Original code written as it used pin.into_floating_input(), not pin.into_pull_up().
+Moreover, suggested way to solve this problem is not correct in my opinion. 
+Due that I rewrite example using pin with pull-up resistors.  
---
+++ 10_led_toggle/src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+use arduino_hal::delay_ms;
+use panic_halt as _;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+
+    let mut led_pin = pins.d13.into_output();
+    led_pin.set_low();
+    let mut btn_pin = pins.d3.into_pull_up_input();
+
+    let mut btn_was_pressed = false;
+    loop {
+        let btn_is_pressed = btn_pin.is_low();
+        if btn_was_pressed && !btn_is_pressed {
+            led_pin.toggle();
+        }
+        btn_was_pressed = btn_is_pressed;
+        // Optional. Adds a more little stability on extreme cases like as
+        // very-very fast pressing on the button using your nail10 
+        delay_ms(10);
+    }
+}
